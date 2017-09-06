2b1bdf75b1bcb53ef9067c68aeb0768748db52c8
Rose Sponseller <rosesponseller@gmail.com>
2017-09-06 16:31:28 -0700

    Add Sign Up feature to landing page html and style the button that connects to it

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import { Profile } from '../models/profile.model';
+import { ProfileService } from '../services/profile.service';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
+  providers: [ProfileService, AuthenticationService]
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent {
+  user;
 
-  constructor() { }
+  private isLoggedIn: Boolean;
+  private userName: String;
 
-  ngOnInit() {
+  constructor(public authService: AuthenticationService) {
+    this.authService.user.subscribe(user => {
+      if (user == null) {
+        this.isLoggedIn = false;
+      } else {
+        this.isLoggedIn = true;
+        this.userName = user.displayName;
+      }
+    });
   }
 
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
