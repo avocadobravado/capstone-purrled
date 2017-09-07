aa88d878ab964d11f8f0a8b70db9ef87638642e8
Rose Sponseller <rosesponseller@gmail.com>
2017-09-07 09:39:14 -0700

    Add min width to landing page images so they look good on all screen sizes

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -6,12 +6,13 @@ import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
 import { Project } from '../models/project.model';
 import { Profile } from '../models/profile.model';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthenticationService {
   user: Observable<firebase.User>;
 
-  constructor(public afAuth: AngularFireAuth) {
+  constructor(public afAuth: AngularFireAuth, public router: Router) {
     this.user = afAuth.authState;
   }
 
@@ -21,57 +22,7 @@ export class AuthenticationService {
 
   logout() {
     this.afAuth.auth.signOut();
+    this.router.navigate(['']);
   }
 
 }
-
-
-//   user: Observable<firebase.User>;
-//   items: FirebaseListObservable<any[]>;
-//   addProfile: FirebaseListObservable<any[]>;
-//
-//   constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
-//     this.user = this.afAuth.authState;
-//
-//   }
-//
-//   login() {
-//     var provider = new firebase.auth.GoogleAuthProvider();
-//
-//     // this.afAuth.auth.signInAnonymously();
-//     firebase.auth().signInWithRedirect(provider).then(function(result) {
-//   // This gives you a Google Access Token. You can use it to access the Google API.
-//     var token = result.credential.accessToken;
-//   // The signed-in user info.
-//     var user = result.user;
-//
-//     var queryObservable = this.database.list('/profiles', {
-//       query: {
-//         orderByChild: 'uid',
-//         equalTo: user.uid
-//       }
-//     });
-//     // if(queryObservable.length < 1) {
-//       this.profileService.addProfile(new Profile (user.uid, this.bio,
-//         this.favorites, this.friends));
-//     // }
-//
-//   });//.catch(function(error) {
-//     // Handle Errors here.
-//     // var errorCode = error.code;
-//     // var errorMessage = error.message;
-//     // // The email of the user's account used.
-//     // var email = error.email;
-//     // // The firebase.auth.AuthCredential type that was used.
-//     // var credential = error.credential;
-//     // ...
-//   //});
-//   }
-//
-//   logout() {
-//     this.afAuth.auth.signOut();
-//   }
-//
-//   authUser() {
-//     return this.user;
-//   }
